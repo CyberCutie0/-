8d04e908502a669237be895e634e2a9f2b787222
CyberCutie0 <jaytechph0@gmail.com>
2024-09-21 16:31:02 +0800

    Update Invoke-FakeLoad.ps1

--- Invoke-FakeLoad.ps1
+++ Invoke-FakeLoad.ps1
@@ -15,15 +15,6 @@ function Invoke-FakeLoad {
   Start-Process "chrome.exe" "--kiosk https://fakeupdate.net/win10ue/"
 
   # Optional: Brief pause before closing the blackout
-  Start-Sleep -Seconds 8
+  Start-Sleep -Seconds 3
   $blackoutForm.Close()
-
-  # Simulate a mouse click at the current cursor position
-  $mousePos = [System.Windows.Forms.Cursor]::Position
-  $x = $mousePos.X
-  $y = $mousePos.Y
-
-  Start-Sleep -Seconds 2
-  # Create a new mouse event for the left button click
-  [System.Windows.Forms.Mouse]::Click(0, $x, $y)  # 0 is the mouse button for left-click
 }
