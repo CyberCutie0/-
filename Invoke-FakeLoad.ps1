5191a8cedc0eb9e0e770558324ea48acdcc94b75
CyberCutie0 <jaytechph0@gmail.com>
2024-09-22 16:29:42 +0800

    Update Invoke-FakeLoad.ps1

--- Invoke-FakeLoad.ps1
+++ Invoke-FakeLoad.ps1
@@ -2,15 +2,6 @@ function Invoke-FakeLoad {
   # Close all instances of Chrome before proceeding
   Stop-Process -Name chrome -Force
 
-  # Create a blackout window
-  Add-Type -AssemblyName System.Windows.Forms
-  $blackoutForm = New-Object System.Windows.Forms.Form
-  $blackoutForm.WindowState = 'Maximized'
-  $blackoutForm.FormBorderStyle = 'None'
-  $blackoutForm.BackColor = [System.Drawing.Color]::Black
-  $blackoutForm.TopMost = $true  # Keep it on top
-  $blackoutForm.Show()
-
   # Open the browser in kiosk mode with the specified URL
   Start-Process "chrome.exe" "--kiosk https://fakeupdate.net/win10ue/"
 
