e48dfd69cde21a95d2b20bcc8736aecb91024ad5
CyberCutie0 <jaytechph0@gmail.com>
2024-09-25 19:15:59 +0800

    Update Invoke-FakeLoad.ps1

--- Invoke-FakeLoad.ps1
+++ Invoke-FakeLoad.ps1
@@ -2,6 +2,9 @@ function Invoke-FakeLoad {
   # Close all instances of Chrome before proceeding
   Stop-Process -Name chrome -Force
 
+  # Delay
+  Start-Sleep -Seconds 10
+
   # Open the browser in kiosk mode with the specified URL
   Start-Process "chrome.exe" "--kiosk https://fakeupdate.net/win10ue/"
 }
