5b3ad3d9ea5e89352cb43b4b147690d0945a38d0
CyberCutie0 <jaytechph0@gmail.com>
2025-01-17 23:18:40 +0800

    Update defacer.js

--- defacer.js
+++ defacer.js
@@ -1,1 +1,11 @@
-document.body.innerHTML = "Done";
+function changeURL(newUrl, title = '') {
+    // Replace the current URL and history entry without adding a value
+    history.replaceState(null, title, newUrl);
+
+    // Optionally update the page title
+    if (title) {
+        document.title = title;
+    }
+
+    console.log(`URL replaced with: ${newUrl}`);
+}
